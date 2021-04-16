6eeed576a349538a6d0b50e63198569a29c9f962
Shimol-Khan <shimolkhan.dev@gmail.com>
2021-04-16 17:19:39 +0800

    Implemented Ask Question Module

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,61 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+
+  def index
+    @questions = Question.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def new
+    @question = current_user.questions.build
+  end
+
+  def edit
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+
+    respond_to do |format|
+      if @question.save
+        format.html { redirect_to @question, notice: "Question was successfully created." }
+        format.json { render :show, status: :created, location: @question }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @question.update(question_params)
+        format.html { redirect_to @question, notice: "Question was successfully updated." }
+        format.json { render :show, status: :ok, location: @question }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @question.destroy
+    respond_to do |format|
+      format.html { redirect_to questions_url, notice: "Question was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def question_params
+      params.require(:question).permit(:title, :body, :user_id)
+    end
+end
